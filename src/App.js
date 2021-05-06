75441d005433fb23861b72ba7de06f9410b0bcd6
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-05-06 09:12:34 -0300

    Footer container completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Main from "./main/Main";
+import Footer from "./Footer";
 
 export default function App() {
     return(
         <>
             <Header />
             <Main />
+            <Footer />
         </>
     );
 }
